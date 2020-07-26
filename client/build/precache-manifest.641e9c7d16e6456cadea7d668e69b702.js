5c3c014df9fbc66335b80f8084498cae62c23b54
Joe Adams <adams.jd10@gmail.com>
2020-07-25 22:57:18 -0700

    update

---
+++ client/build/precache-manifest.641e9c7d16e6456cadea7d668e69b702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b2dcebec852bf0ae5a6",
+    "url": "/static/css/main.f4b6d383.chunk.css"
+  },
+  {
+    "revision": "2b2dcebec852bf0ae5a6",
+    "url": "/static/js/main.293651c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da57d359fc4b4fc0e63b",
+    "url": "/static/js/2.c455dd94.chunk.js"
+  },
+  {
+    "revision": "3b3eea70f84f977e56cbaef5851d09ea",
+    "url": "/index.html"
+  }
+];
